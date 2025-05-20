b1a66838fcd1af3c83490cf001a695556597b899
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-20 15:27:30 +0530

    Update variable.tf

--- infra/variable.tf
+++ infra/variable.tf
@@ -36,14 +36,13 @@ variable "sg_tags" {
 }
 
 #############RDS
+variable "region" {}
 variable "rds_username" {}
 variable "rds_instance_identifier" {}
-variable "rds_instance_engine" {}
 variable "rds_instance_class" {}
-variable "rds_instance_allocated_storage" {}
+variable "rds_instance_db_name" {}
 variable "rds_instance_multi_az" { type = bool }
 variable "rds_instance_storage_encrypted" { type = bool }
-variable "rds_instance_db_name" {}
 variable "parameter_group_name" {}
 variable "parameter_group_family" {}
 variable "subnet_ids" { type = list(string) }
