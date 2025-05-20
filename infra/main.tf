9efd023db753e6cc2264338f8b07c3cae21f68d7
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-20 15:26:16 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,19 +8,17 @@ module "sg_rds" {
 }
 
 module "aurora_postgres" {
-  source = "../modules/rds"
-  rds_username                = var.rds_username
-  rds_instance_identifier     = var.rds_instance_identifier
-  rds_instance_engine         = var.rds_instance_engine
-  rds_instance_class          = var.rds_instance_class
-  rds_instance_allocated_storage = var.rds_instance_allocated_storage
-  rds_instance_multi_az       = var.rds_instance_multi_az
+  source                        = "./modules/aurora_postgres"
+  rds_username                  = var.rds_username
+  rds_instance_identifier       = var.rds_instance_identifier
+  rds_instance_class            = var.rds_instance_class
+  rds_instance_db_name          = var.rds_instance_db_name
+  rds_instance_multi_az         = var.rds_instance_multi_az
   rds_instance_storage_encrypted = var.rds_instance_storage_encrypted
-  rds_instance_db_name        = var.rds_instance_db_name
-  parameter_group_name        = var.parameter_group_name
-  parameter_group_family      = var.parameter_group_family
-  subnet_ids                  = var.subnet_ids
-  subnet_group_name           = var.subnet_group_name
-  vpc_security_group_ids      = [module.sg_rds.sg_out]
-  rds_instance_tags           = var.rds_instance_tags
+  parameter_group_name          = var.parameter_group_name
+  parameter_group_family        = var.parameter_group_family
+  subnet_ids                    = var.subnet_ids
+  subnet_group_name             = var.subnet_group_name
+  vpc_security_group_ids        = var.vpc_security_group_ids
+  rds_instance_tags             = var.rds_instance_tags
 }
