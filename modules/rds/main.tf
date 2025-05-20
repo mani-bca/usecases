fc528b07d4dfdd66f1ae4854dcc2418ed9813113
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-20 15:31:43 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,23 +1,20 @@
+resource "aws_secretsmanager_secret" "rds_secret" {}
 
-resource "aws_secretsmanager_secret" "rds_secret" {
- 
+resource "random_password" "rds_password" {
+  length  = 16
+  special = true
 }
- 
+
 resource "aws_secretsmanager_secret_version" "rds_secret_version" {
   secret_id     = aws_secretsmanager_secret.rds_secret.id
   secret_string = jsonencode({
     username = var.rds_username
     password = random_password.rds_password.result
   })
- 
+
   depends_on = [aws_secretsmanager_secret.rds_secret]
 }
- 
-resource "random_password" "rds_password" {
-  length  = 16
-  special = true
-}
- 
+
 resource "aws_rds_cluster" "aurora_cluster" {
   cluster_identifier      = var.rds_instance_identifier
   engine                  = "aurora-postgresql"
@@ -28,7 +25,6 @@ resource "aws_rds_cluster" "aurora_cluster" {
   db_subnet_group_name    = aws_db_subnet_group.generic_subnet_group.name
   vpc_security_group_ids  = var.vpc_security_group_ids
   storage_encrypted       = var.rds_instance_storage_encrypted
-  # kms_key_id            = var.rds_instance_kms_key_id
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.aurora_pg_param_group.name
   tags                    = var.rds_instance_tags
   copy_tags_to_snapshot   = true
@@ -36,7 +32,6 @@ resource "aws_rds_cluster" "aurora_cluster" {
   depends_on = [aws_secretsmanager_secret_version.rds_secret_version]
 }
 
-# NEW RESOURCE: Aurora Cluster Instance
 resource "aws_rds_cluster_instance" "aurora_instance" {
   count              = var.rds_instance_multi_az ? 2 : 1
   identifier         = "${var.rds_instance_identifier}-${count.index + 1}"
@@ -46,10 +41,9 @@ resource "aws_rds_cluster_instance" "aurora_instance" {
   tags               = var.rds_instance_tags
 }
 
-# CHANGED: Aurora parameter group resource
 resource "aws_rds_cluster_parameter_group" "aurora_pg_param_group" {
   name   = var.parameter_group_name
-  family = var.parameter_group_family # Example: aurora-postgresql15
+  family = var.parameter_group_family
   tags   = var.rds_instance_tags
 }
 
@@ -58,28 +52,3 @@ resource "aws_db_subnet_group" "generic_subnet_group" {
   subnet_ids = var.subnet_ids
   tags       = var.rds_instance_tags
 }
-
- 
-# resource "aws_db_proxy" "my_db_proxy" {
-#   name                     = var.my_db_proxy
-#   engine_family            = "POSTGRESQL"
-#   # role_arn                 = var.proxy_role_arn
-#   vpc_security_group_ids   = [module.securitygroups.sg_out]
-#   vpc_subnet_ids           = var.subnet_ids
-#   auth {
-#     secret_arn = aws_secretsmanager_secret.rds_secret.arn
-#     iam_auth   = "DISABLED"
-#   }
-#   idle_client_timeout       = 1800  
-#   require_tls               = true  
-#   debug_logging             = true  
-#   tags                      = var.rds_instance_tags
-# }
- 
-# resource "aws_db_proxy_target" "my_rds_proxy_target" {
-#   db_proxy_name        = aws_db_proxy.my_db_proxy.name
-#   target_group_name    = "default"
-#   db_instance_identifier = aws_db_instance.my_rds_instance.identifier
- 
-#   depends_on = [aws_db_instance.my_rds_instance, aws_db_proxy.my_db_proxy]
-# }
