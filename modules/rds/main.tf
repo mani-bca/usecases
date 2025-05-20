3056d9b331dd74dadbee5f9d41ffbaba70e026bc
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-20 08:19:38 +0000

    fesd

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,11 +1,11 @@
-module "securitygroups" {
-  source            = "../networking/securitygroups"
-  name              = var.sg_name
-  description       = var.sg_description
-  vpc_id            = var.sg_vpc_id
-  sg_ingress_rules  = var.sg_ingress_rules
-  tags              = var.sg_tags
-}
+# module "securitygroups" {
+#   source            = "../networking/securitygroups"
+#   name              = var.sg_name
+#   description       = var.sg_description
+#   vpc_id            = var.sg_vpc_id
+#   sg_ingress_rules  = var.sg_ingress_rules
+#   tags              = var.sg_tags
+# }
  
 resource "aws_secretsmanager_secret" "rds_secret" {
  
