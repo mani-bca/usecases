e89592f3ec7ea6ba36df61592de48f26b2c50cc8
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-22 13:01:50 +0530

    rootmain.tf

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,5 +1,9 @@
-variable "raw_bucket_name" { type = string }
-variable "processed_bucket_name" { type = string }
+variable "raw_bucket_name" { 
+  type = string 
+}
+variable "processed_bucket_name" { 
+  type = string 
+}
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
@@ -30,25 +34,60 @@ variable "name" {
   type        = string
 }
 
-variable "db_name" { type = string }
-variable "db_username" { type = string }
-variable "db_password" { type = string; sensitive = true }
-variable "db_instance_class" { type = string }
-variable "db_secret_name" { type = string }
+variable "db_name" { 
+  type = string 
+}
+variable "db_username" { 
+  type = string 
+}
+variable "db_password" { 
+  type = string 
+  sensitive = true 
+}
+variable "db_instance_class" { 
+  type = string 
+}
+variable "db_secret_name" { 
+  type = string 
+}
 
-variable "lambda_code_bucket" { type = string }
-variable "ingest_lambda_key" { type = string }
-variable "search_lambda_key" { type = string }
+variable "lambda_code_bucket" { 
+  type = string 
+}
+variable "ingest_lambda_key" { 
+  type = string 
+}
+variable "search_lambda_key" { 
+  type = string 
+}
 
-variable "ingest_lambda_name" { type = string }
-variable "search_lambda_name" { type = string }
-variable "ingest_lambda_handler" { type = string }
-variable "search_lambda_handler" { type = string }
+variable "ingest_lambda_name" { 
+  type = string 
+}
+variable "search_lambda_name" { 
+  type = string 
+}
+variable "ingest_lambda_handler" { 
+  type = string 
+}
+variable "search_lambda_handler" { 
+  type = string 
+}
 
-variable "lambda_runtime" { type = string }
-variable "lambda_role_name" { type = string }
-variable "lambda_policy_arns" { type = list(string) }
+variable "lambda_runtime" { 
+  type = string 
+}
+variable "lambda_role_name" { 
+  type = string 
+}
+variable "lambda_policy_arns" { 
+  type = list(string) 
+}
 
-variable "api_name" { type = string }
+variable "api_name" { 
+  type = string 
+}
 
-variable "tags" { type = map(string) }
+variable "tags" { 
+  type = map(string) 
+}
