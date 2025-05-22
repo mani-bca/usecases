0c6839ceb160e73adf82eb4a99337aaeac3aca93
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-22 14:48:10 +0530

    engine version update

--- modules/rds_postgresql/variable.tf
+++ modules/rds_postgresql/variable.tf
@@ -13,7 +13,7 @@ variable "db_instance_class" {
 }
 variable "engine_version" { 
     type = string 
-    default = "15.3" 
+    default = "15.9" 
 }
 variable "allocated_storage" { 
     type = number 
