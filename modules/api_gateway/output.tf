35d13f10452a9ca98320a4dfb4ac5446f98af6c0
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-22 11:51:50 +0530

    Update output.tf

--- modules/api_gateway/output.tf
+++ modules/api_gateway/output.tf
@@ -1,7 +1,5 @@
-output "rds_endpoint" {
-  value = aws_db_instance.this.endpoint
+output "api_url" {
+  value = "https://${aws_apigatewayv2_api.this.api_id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${var.stage_name}"
 }
 
-output "rds_port" {
-  value = aws_db_instance.this.port
-}
+data "aws_region" "current" {}
