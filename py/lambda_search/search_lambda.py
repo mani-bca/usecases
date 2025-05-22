f6a64044ab905ca4f9b2c4399f4bb73189ce07be
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-22 12:14:36 +0530

    Create lambda_search/search_lambda.py

---
+++ py/lambda_search/search_lambda.py
@@ -0,0 +1,45 @@
+import os
+import boto3
+import psycopg2
+from sentence_transformers import SentenceTransformer
+
+SECRET_NAME = os.environ['DB_SECRET_NAME']
+REGION = os.environ.get('AWS_REGION', 'us-east-1')
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def get_db_credentials():
+    client = boto3.client('secretsmanager', region_name=REGION)
+    secret = client.get_secret_value(SecretId=SECRET_NAME)
+    return eval(secret['SecretString'])
+
+def connect_db():
+    creds = get_db_credentials()
+    return psycopg2.connect(
+        host=creds['host'],
+        database=creds['dbname'],
+        user=creds['username'],
+        password=creds['password'],
+        port=creds['port']
+    )
+
+def lambda_handler(event, context):
+    query = event.get("query", "")
+    if not query:
+        return {"error": "Missing query"}
+
+    embedding = model.encode([query])[0]
+
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute(\"\"\"
+        SELECT chunk, embedding <#> %s AS distance
+        FROM documents
+        ORDER BY distance
+        LIMIT 5;
+    \"\"\", (embedding.tolist(),))
+
+    results = [{"chunk": row[0], "score": row[1]} for row in cur.fetchall()]
+    cur.close()
+    conn.close()
+    return {"results": results}
