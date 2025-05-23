902f20fb62fbf5ecc1f1afc15b2a7ad3f71cac40
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-23 08:03:16 +0530

    Update output.tf

--- modules/api_gateway/output.tf
+++ modules/api_gateway/output.tf
@@ -1,8 +1,11 @@
-# output "api_url" {
-#   value = "https://${aws_apigatewayv2_api.this.api_id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${var.stage_name}"
-# }
+
+
+data "aws_region" "current" {}
+
 output "api_url" {
   value = "https://${aws_apigatewayv2_api.this.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${var.stage_name}"
 }
 
-data "aws_region" "current" {}
+output "api_id" {
+  value = aws_apigatewayv2_api.this.id
+}
