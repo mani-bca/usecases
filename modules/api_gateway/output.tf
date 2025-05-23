fa4b842e401c0f88799754ccd4343b143b2345e7
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-23 08:12:11 +0530

    removed data

--- modules/api_gateway/output.tf
+++ modules/api_gateway/output.tf
@@ -1,6 +1,6 @@
 
 
-data "aws_region" "current" {}
+# data "aws_region" "current" {}
 
 output "api_url" {
   value = "https://${aws_apigatewayv2_api.this.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${var.stage_name}"
