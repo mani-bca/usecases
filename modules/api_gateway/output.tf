83841b049ad023165383f669944fbe697cc2e17c
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-25 14:11:45 +0530

    Update output.tf

--- modules/api_gateway/output.tf
+++ modules/api_gateway/output.tf
@@ -1,11 +1,3 @@
-
-
-# data "aws_region" "current" {}
-
-output "api_url" {
-  value = "https://${aws_apigatewayv2_api.this.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${var.stage_name}"
-}
-
-output "api_id" {
-  value = aws_apigatewayv2_api.this.id
+output "invoke_url" {
+  value = "https://${aws_apigatewayv2_api.this.id}.execute-api.${var.region}.amazonaws.com/${var.stage_name}"
 }
