6d9cf040bf2d87d5dbf78da9d8b8756a454e4395
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-25 18:46:48 +0530

    fets

--- infra/variable.tf
+++ infra/variable.tf
@@ -61,10 +61,10 @@ variable "domain_prefix" {
   type        = string
 }
 
-# variable "callback_urls" {
-#   description = "List of callback URLs for the Cognito User Pool app client."
-#   type        = list(string)
-# }
+variable "callback_urls" {
+  description = "List of callback URLs for the Cognito User Pool app client."
+  type        = list(string)
+}
 
 variable "logout_urls" {
   description = "List of logout URLs for the Cognito User Pool app client."
