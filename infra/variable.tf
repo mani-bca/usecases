0df2840e202ed3133f7c730897c301c5d84bfd7b
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-25 14:25:29 +0530

    Update variable.tf

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,125 +1,28 @@
-variable "raw_bucket_name" { 
-  type = string 
+variable "function_name" {}
+variable "handler" {}
+variable "runtime" {}
+variable "source_path" {}
+variable "memory_size" {
+  type    = number
+  default = 128
 }
-variable "processed_bucket_name" { 
-  type = string 
+variable "timeout" {
+  type    = number
+  default = 3
 }
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
+variable "layers" {
+  type    = list(string)
+  default = []
 }
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  type        = list(string)
+variable "environment_variables" {
+  type    = map(string)
+  default = {}
 }
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
+variable "architecture" {
+  type    = string
+  default = "x86_64"
 }
-
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-}
-
-variable "create_nat_gateway" {
-  description = "Whether to create a NAT Gateway"
-  type        = bool
-}
-variable "name" {
-  description = "VPC name"
-  type        = string
-}
-
-variable "db_name" { 
-  type = string 
-}
-variable "db_username" { 
-  type = string 
-}
-variable "db_password" { 
-  type = string 
-  sensitive = true 
-}
-variable "db_instance_class" { 
-  type = string 
-}
-variable "db_secret_name" { 
-  type = string 
-}
-
-variable "lambda_code_bucket" { 
-  type = string 
-}
-variable "ingest_lambda_key" { 
-  type = string 
-}
-variable "search_lambda_key" { 
-  type = string 
-}
-
-variable "ingest_lambda_name" { 
-  type = string 
-}
-variable "search_lambda_name" { 
-  type = string 
-}
-# variable "ingest_lambda_handler" { 
-#   type = string 
-# }
-# variable "search_lambda_handler" { 
-#   type = string 
-# }
-
-variable "lambda_runtime" { 
-  type = string 
-}
-variable "lambda_role_name" { 
-  type = string 
-}
-variable "lambda_policy_arns" { 
-  type = list(string) 
-}
-
-variable "api_name" { 
-  type = string 
-}
-
-variable "tags" { 
-  type = map(string) 
-}
-
-variable "lambda_egress_rules" {
-  description = "Egress rules for Lambda SG"
-  type = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks = optional(list(string))
-  }))
-}
-
-
-variable "rds_ingress_rules" {
-  description = "Ingress rules for RDS SG"
-  type = list(object({
-    from_port       = number
-    to_port         = number
-    protocol        = string
-    security_groups = optional(list(string))
-    cidr_blocks     = optional(list(string))
-  }))
-}
-
-variable "rds_egress_rules" {
-  description = "Egress rules for RDS SG"
-  type = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks = optional(list(string))
-  }))
+variable "lambda_role_name" {}
+variable "lambda_policy_arns" {
+  type = list(string)
 }
