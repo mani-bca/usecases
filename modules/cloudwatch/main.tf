36577a21b357d82346a368a19802bd23844366b8
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-27 21:01:22 +0530

    Update main.tf

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -16,9 +16,23 @@ resource "aws_iam_role" "cloudtrail_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "cloudwatch_attach" {
-  role       = aws_iam_role.cloudtrail_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCloudTrailFullAccess"
+resource "aws_iam_role_policy" "cloudwatch_logs" {
+  name = "AllowCloudTrailToWriteToCloudWatch"
+  role = aws_iam_role.cloudtrail_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:PutLogEvents",
+          "logs:CreateLogStream"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_cloudwatch_log_metric_filter" "login_filter" {
