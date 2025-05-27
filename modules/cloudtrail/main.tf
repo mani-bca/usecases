d45bc935ae6c93e23297f803e98de4cef9539f93
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-27 17:15:26 +0530

    Create cloudtrail/main.tf

---
+++ modules/cloudtrail/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "trail_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_cloudtrail" "trail" {
+  name                          = var.trail_name
+  s3_bucket_name                = aws_s3_bucket.trail_bucket.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  cloud_watch_logs_group_arn    = var.cloudwatch_log_group_arn
+  cloud_watch_logs_role_arn     = var.cloudwatch_role_arn
+  is_logging                    = true
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+}
