e414dd987163146cc38244e3fa52028bf38c085b
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-05-29 14:03:56 +0530

    Update variable.tf

--- modules/api_gateway/variable.tf
+++ modules/api_gateway/variable.tf
@@ -15,3 +15,25 @@ variable "tags" {
 variable "lambda_function_name" {
   type = string
 }
+######
+variable "api_name" {
+  type = string
+}
+
+variable "stage_name" {
+  type    = string
+  default = "$default"
+}
+
+variable "routes" {
+  description = "List of route definitions: each with lambda_arn, lambda_name, and path"
+  type = list(object({
+    path              = string
+    lambda_function_arn  = string
+    lambda_function_name = string
+  }))
+}
+
+variable "tags" {
+  type = map(string)
+}
