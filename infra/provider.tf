4e06b91266674163a0088218547a55a9dbe6b133
mani-bca <manivasagan.p@hcltech.com>
2025-06-04 16:13:01 +0530

    dddd

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "dev-terraform-state-platform"
-    key            = "usecase5/terraform.tfstate"
+    key            = "usecase20/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
     encrypt        = true
