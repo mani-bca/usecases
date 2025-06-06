8b855defdc8d9e62f810fb90a6191693bec6eed6
mani-bca <manivasagan.p@hcltech.com>
2025-06-06 10:34:08 +0530

    cdcc

--- modules/4iam_role/variable.tf
+++ modules/4iam_role/variable.tf
@@ -1,22 +1,22 @@
-variable "role_name" { 
-    type = string 
-}
-variable "policy_arns" { 
-    type = list(string) 
-}
-variable "tags" { 
-    type = map(string) 
-}
-variable "inline_policy" {
-  description = "IAM inline policy in JSON format"
-  type        = string
-  default     = null
-}
+# variable "role_name" { 
+#     type = string 
+# }
+# variable "policy_arns" { 
+#     type = list(string) 
+# }
+# variable "tags" { 
+#     type = map(string) 
+# }
+# variable "inline_policy" {
+#   description = "IAM inline policy in JSON format"
+#   type        = string
+#   default     = null
+# }
 
-variable "assume_role_policy" {
-  description = "The assume role policy document in JSON format. This defines who or what can assume this role."
-  type        = string
-}
+# variable "assume_role_policy" {
+#   description = "The assume role policy document in JSON format. This defines who or what can assume this role."
+#   type        = string
+# }
 ##################################3below is new update
 variable "role_name" {
   description = "Name of the IAM role."
